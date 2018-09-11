8bf2c576a1f7705ef47c53d9c5d993195aaa4551
Aspen James <aspenjames@tqca.org>
2018-09-10 19:57:28 -0700

    draw initial API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :coffee_shops, :only => [:create, :update, :show, :destroy] do
+    resources :espressos, :only => [:create, :update, :show, :index, :destory]
+    resources :origins, :only => [:create, :update, :show, :index, :destory]
+  end
 end
