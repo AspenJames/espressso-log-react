24099dd37a0cd13f8000cc52dfba37f731f1da43
Aspen James <aspenjames@tqca.org>
2018-09-13 11:41:45 -0700

    remove email and password fields from coffee_shops

--- db/migrate/20180908194309_create_coffee_shops.rb
+++ db/migrate/20180908194309_create_coffee_shops.rb
@@ -3,8 +3,6 @@ class CreateCoffeeShops < ActiveRecord::Migration[5.2]
     create_table :coffee_shops do |t|
       t.string :name
       t.string :address
-      t.string :email
-      t.string :password_digest
 
       t.timestamps
     end
