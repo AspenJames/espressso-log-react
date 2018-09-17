decdbd09ec1d6dd591e851ee6dc89903055fd75c
Aspen James <aspenjames@tqca.org>
2018-09-16 19:17:28 -0700

    create and connect redux store + thunk middleware

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import thunk from 'redux-thunk';
+
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from './components/Login';
 
+const store = createStore(rootReducer, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), 
+  applyMiddleware(thunk));
+
 ReactDOM.render((
-  <Router>
-    <React.Fragment>
-      <Route path='/' component={App} />
-      <Route path='/login' component={Login} />
-    </React.Fragment>
-  </Router>),
+  <Provider store={store}>
+    <Router>
+      <React.Fragment>
+        <Route path='/' component={App} />
+        <Route path='/login' component={Login} />
+      </React.Fragment>
+    </Router>
+  </Provider>),
   document.getElementById('root')
 );
 registerServiceWorker();
