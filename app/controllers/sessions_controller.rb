47b4c206845bf2b89a03607395ce6bb331609436
Aspen James <aspenjames@tqca.org>
2018-09-17 14:49:12 -0700

    namespace user params, add sessions#destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_by(:email => params[:email]).try(:authenticate, params[:password])
+    @user = User.find_by(:email => params[:user][:email]).try(:authenticate, params[:user][:password])
     if @user
       session[:user_id] = @user.id 
       render :json => @user
@@ -10,5 +10,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    @user = User.find(params[:user][:id])
+    session.clear
+    render :json => {"message": "#{@user.name} logged out"}
   end
 end
