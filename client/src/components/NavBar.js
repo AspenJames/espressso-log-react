a75d6571dfc98a038800be465b027bde608bc683
Aspen James <aspenjames@tqca.org>
2018-09-17 14:52:06 -0700

    render nav links based on logged-in status

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const NavBar = () => {
+const NavBar = props => {
+  const renderUserLinks = () => {
+    if (props.user === '') {
+      return (
+        <React.Fragment>
+        <NavLink to='/login'    exact>Login</NavLink><span> | </span>
+        <NavLink to='/signup'   exact>Signup</NavLink>
+        </React.Fragment>
+      )
+    } else {
+      return (
+        <a href='api/logout' onClick={props.handleLogout}>Logout</a>
+      )
+    }
+  }
+
     return (
         <div className="navbar" >
             <NavLink to='/'         exact>Home</NavLink><span> | </span>
-            <NavLink to='/login'    exact>Login</NavLink><span> | </span>
-            <NavLink to='/signup'   exact>Signup</NavLink>
+            {renderUserLinks()}
         </div>
     );
 }
+
 
 export default NavBar;
