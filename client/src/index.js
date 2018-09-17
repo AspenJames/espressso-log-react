44ffb9cdd2938f226c9e9b2684265fd6b361e741
Aspen James <aspenjames@tqca.org>
2018-09-16 20:53:37 -0700

    move routing to App component

--- client/src/index.js
+++ client/src/index.js
@@ -8,21 +8,13 @@ import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Login from './components/Login';
-
 const store = createStore(rootReducer, 
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), 
   applyMiddleware(thunk));
 
 ReactDOM.render((
   <Provider store={store}>
-    <Router>
-      <React.Fragment>
-        <Route path='/' component={App} />
-        <Route path='/login' component={Login} />
-      </React.Fragment>
-    </Router>
+    <App />
   </Provider>),
   document.getElementById('root')
 );
