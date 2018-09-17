72ea280174685ca84f47401bda5528528e366017
Aspen James <aspenjames@tqca.org>
2018-09-16 22:45:28 -0700

    add validations on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password 
+  validates :name, :email, :presence => true, :uniqueness => true
 
   has_many :coffee_shop_users
   has_many :coffee_shops, :through => :coffee_shop_users
