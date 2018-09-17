556b6d2885fc96df77801eedfb9709fe68c1772c
Aspen James <aspenjames@tqca.org>
2018-09-16 18:56:36 -0700

    move Login component to components directory

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Login from './Login';
+import Login from './components/Login';
 
 ReactDOM.render((
   <Router>
