7a733700b1bb743d9d3c7cd9c42dfed0765a8a0d
Aspen James <aspenjames@tqca.org>
2018-09-16 20:41:20 -0700

    add basic login logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,12 @@
 class SessionsController < ApplicationController
   def create
+    @user = User.find_by(:email => params[:email]).try(:authenticate, params[:password])
+    if @user
+      session[:user_id] = @user.id 
+      render :json => @user
+    else 
+      render :json => {"error": "not found"}
+    end
   end
 
   def destroy
