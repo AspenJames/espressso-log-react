a5e1f9b5d16c725c1b54da1615a97d2c55deb4c6
Aspen James <aspenjames@tqca.org>
2018-09-16 23:11:29 -0700

    refactor error JSON object

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id 
       render :json => @user
     else 
-      render :json => {"error": "not found"}
+      render :json => {"errors": "Email and/or password incorrect"}
     end
   end
 
