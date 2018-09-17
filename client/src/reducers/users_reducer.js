362d505455a88413512edcd9e4e290f2e37f018f
Aspen James <aspenjames@tqca.org>
2018-09-17 10:23:42 -0700

    init add user action

--- client/src/reducers/users_reducer.js
+++ client/src/reducers/users_reducer.js
@@ -3,7 +3,9 @@ export default function usersReducer(state = {
 }, action) {
     switch (action.type) {
         case "ADD_USER":
+        debugger;
             return {user: {
+              id: action.user.id,
               name: action.user.name,
               email: action.user.email
             }};
