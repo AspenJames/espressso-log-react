9463064c118316a9011fedcf5cf447c0be3f9b62
Aspen James <aspenjames@tqca.org>
2018-09-18 21:25:45 -0700

    add method to return shops for which user is admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
   has_many :coffee_shops, :through => :coffee_shop_users
   has_many :espressos
   has_many :origins, :through => :espressos
+
+  def admin_shops
+    # returns an ActiveRecord::Relation object containing
+    # all coffee shops for which the user is admin
+    CoffeeShop.joins(:coffee_shop_users)
+      .where("coffee_shop_users.user_id = #{self.id}")
+      .where("coffee_shop_users.admin = true")
+  end
 end
