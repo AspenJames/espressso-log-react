f166def583848302e2152445d22686e67e6f33b6
Aspen James <aspenjames@tqca.org>
2018-09-20 14:51:29 -0700

    Add helper methods

--- app/models/coffee_shop.rb
+++ app/models/coffee_shop.rb
@@ -4,4 +4,14 @@ class CoffeeShop < ApplicationRecord
 
   has_many :coffee_shop_users
   has_many :users, :through => :coffee_shop_users
+
+  def users_pending_approval 
+    User.joins(:coffee_shop_users)
+      .where("coffee_shop_users.coffee_shop_id = #{self.id}")
+      .where("coffee_shop_users.approved = false")
+  end
+
+  def user_exists?(user)
+    user.coffee_shops.include?(self)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,27 @@ class User < ApplicationRecord
       .where("coffee_shop_users.user_id = #{self.id}")
       .where("coffee_shop_users.admin = true")
   end
+
+  def is_admin?
+    self.admin_shops.length > 0
+  end
+
+  def pending_approvals
+    CoffeeShop.joins(:coffee_shop_users)
+      .where("coffee_shop_users.user_id = #{self.id}")
+      .where("coffee_shop_users.approved = false")
+  end
+
+  def approve_user(coffee_shop, user)
+    # Exit if calling user is not admin of the coffee_shop
+    return false if !self.admin_shops.include?(coffee_shop)
+    # Exit if passed in user is not present in the users for 
+    # the passed in coffee_shop
+    return false if !coffee_shop.user_exists?(user)
+    # Grab the coffee_shop_user entry for passed in args
+    c = CoffeeShopUser.where("coffee_shop_id = #{coffee_shop.id}")
+          .where("user_id = #{user.id}")
+    binding.pry
+    c.update(:approved => true)
+  end
 end
