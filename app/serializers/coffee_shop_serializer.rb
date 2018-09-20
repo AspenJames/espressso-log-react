0174cb9e68ebed77a5b279f94d0bcf2c60bbe92b
Aspen James <aspenjames@tqca.org>
2018-09-20 16:05:31 -0700

    add users_pending_approval to coffee shop serializer

--- app/serializers/coffee_shop_serializer.rb
+++ app/serializers/coffee_shop_serializer.rb
@@ -1,5 +1,11 @@
 class CoffeeShopSerializer < ActiveModel::Serializer
   attributes :id, :name, :address
+  has_many :users_pending_approval
   has_many :espressos
   has_many :users, :through => :coffee_shop_users
+
+  def users_pending_approval
+    object.users_pending_approval.map{|u| {"id": u.id, "name": u.name}}
+  end
+
 end
