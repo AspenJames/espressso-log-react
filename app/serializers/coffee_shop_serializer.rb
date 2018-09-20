37c56413f9d8717f49ccba62a1802cee3a7d0da9
Aspen James <aspenjames@tqca.org>
2018-09-20 16:15:56 -0700

    refactor

--- app/serializers/coffee_shop_serializer.rb
+++ app/serializers/coffee_shop_serializer.rb
@@ -1,11 +1,11 @@
 class CoffeeShopSerializer < ActiveModel::Serializer
   attributes :id, :name, :address
-  has_many :users_pending_approval
+  has_many :users_pending_approval, :serializer => UserSerializer
   has_many :espressos
   has_many :users, :through => :coffee_shop_users
 
   def users_pending_approval
-    object.users_pending_approval.map{|u| {"id": u.id, "name": u.name}}
+    object.users_pending_approval
   end
 
 end
