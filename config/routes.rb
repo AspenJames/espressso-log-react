c395f2f2bc3014a76442f88d4f1aadc0ca1e5527
Aspen James <aspenjames@tqca.org>
2018-09-24 17:04:07 -0700

    add routes for coffee shop index and join

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope '/api' do
-    resources :coffee_shops, :only => [:create, :update, :show, :destroy] do
+    resources :coffee_shops, :only => [:create, :update, :show, :index, :destroy] do
       resources :espressos, :only => [:create, :update, :show, :index, :destory]
       resources :origins, :only => [:create, :update, :show, :index, :destory]
     end
 
     resources :users, :only => [:create, :update, :show, :index, :destroy]
+    resources :coffee_shop_users, :only => [:create, :update, :destroy]
 
     post '/login' => 'sessions#create'
     post '/logout' => 'sessions#destroy'
